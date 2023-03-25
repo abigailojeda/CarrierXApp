3455f45b02ff2020dd9b5752e32014ad1b2a9770
abigailojeda <abigail.arte@gmail.com>
2023-03-25 15:00:41 +0000

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const App = () => {
+  return (
+    <View>
+      <Text>
+        Carrier X App
+      </Text>
+    </View>
+  )
+}
